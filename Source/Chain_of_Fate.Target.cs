2f7fc5e62b1d0e3a78a80c48c53e0e9eb15658e1
MAIN_PC\dbdal <dbdaldrb1@gmail.com>
2026-06-11 00:03:20 +0900

    5,7 update

--- Source/Chain_of_Fate.Target.cs
+++ Source/Chain_of_Fate.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class Chain_of_FateTarget : TargetRules
 {
-	public Chain_of_FateTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
-		ExtraModuleNames.Add("Chain_of_Fate");
-	}
+    public Chain_of_FateTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+
+        ExtraModuleNames.Add("Chain_of_Fate");
+    }
 }
